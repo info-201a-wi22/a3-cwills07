b22a958a436a461d395b955235dc5d10eb640196
cwills07 <cwills07@uw.edu>
2022-02-25 20:32:57 -0800

    5

--- source/comparison_chart.R
+++ source/comparison_chart.R
@@ -1,5 +1,8 @@
 # Chart Comparing the number of Black People Incarcerated to the number of White People Incarcerated within different urbancities in 2009
 
+incaceration_file <- "http://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv"
+incarceration_trends <- read.csv(url(incaceration_file), stringsAsFactors = FALSE)
+
 df2 <- incarceration_trends %>%
   filter(white_prison_adm > 0, black_prison_adm > 0, year == "2009") %>%
   select(white_prison_adm, black_prison_adm, urbanicity)
